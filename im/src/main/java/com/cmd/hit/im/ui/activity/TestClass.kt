d8a3c2898fe63325dab8275227e0fcf5006c2518
cenmingdi <cenmingdi@bytedance.com>
2019-10-01 17:30:58 +0800

    删除module.xml中多余项，修改个module中ignore文件

---
+++ im/src/main/java/com/cmd/hit/im/ui/activity/TestClass.kt
@@ -0,0 +1,27 @@
+package com.cmd.hit.im.ui.activity
+
+import android.content.Context
+import com.cmd.hit.im.base.MyApplication.mContext
+
+class TestClass(mcontext: String, id: Int, postion: Int) {
+    val context  = mcontext
+
+    init{
+    }
+
+    constructor(mcontext: String, id: Int): this(mcontext, id, 0){
+        println("constructor 2")
+    }
+
+    constructor(mcontext: String): this(mcontext, 0){
+        println("constructor 1")
+    }
+
+    fun a(){
+
+    }
+}
+
+fun main(){
+    val test = TestClass("hahaha")
+}
