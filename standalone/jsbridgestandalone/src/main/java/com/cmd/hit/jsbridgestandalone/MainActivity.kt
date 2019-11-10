a53f1bcdcddeac36e49fc4d38a886d62da62f5b5
[GtLeaf] <962755614@qq.com>
2019-11-10 20:29:37 +0800

    增加jsBridge模块

---
+++ standalone/jsbridgestandalone/src/main/java/com/cmd/hit/jsbridgestandalone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cmd.hit.jsbridgestandalone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
